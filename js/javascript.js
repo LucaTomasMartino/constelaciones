cb63bff512661f0c1d278f8d0dce08af3f15fca1
lucatomasmartino <martinolucatomas@gmail.com>
2024-03-19 06:02:18 -0300

    cartel bienvenida con boton

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,13 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.getElementById("overlay");
+    const acceptButton = document.getElementById("accept-button");
+
+    acceptButton.addEventListener("click", function() {
+        overlay.style.display = "none";
+    });
+    
+});
